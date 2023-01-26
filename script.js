1468038fc81ad339867f89e20ae3dc5f9f38cee1
Megan <esler6724@gmail.com>
2023-01-25 19:55:33 -0800

    added border to the image

--- script.js
+++ script.js
@@ -12,13 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchCatImage()
 })
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame) 
 
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
 })
 
+//this function is called when startbutton is clicked
+//after start button is clicked, the 'hide' classList is added which makes the button disappear
+//questions array of objs are then shuffled so that the order of the questions is random
+//question container element is unhidden
+//set next question function is called
 function startGame() {
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
@@ -27,12 +32,23 @@ function startGame() {
     setNextQuestion()
 }
 
+//this function triggers a few other unique functions
+//reset state - this clears out the answer and colors set from the previous question
+//get us a new cute cate image thank-YOU!
+//show the next question in the new shuffled question array
 function setNextQuestion() {
     resetState()
     fetchCatImage()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+//this function is going to show the next question
+//first it adds the specific question as innertext to the element
+//next a button variable is created and an element 'button'
+//then for each answer (there are two) the button inner text is updated to those answers
+//class of 'btn' is assigned so formatting is identical
+//then, the if evaluates which answer of the two is "correct" and if it is, defines answer.correct
+//button event listener when its clicked will call the select answer function and append the button variable to the answer button element
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -47,6 +63,10 @@ function showQuestion(question) {
     })
 }
 
+//this function resets the elements
+//first it calls the clearStatusClass function (this takes care of removing things and resetting them)
+//then the next button is hidden again until an answer is clicked
+//a while loop is used to check for the presence of a correct/wrong child on the answer button element and remove if there
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
@@ -56,6 +76,11 @@ function resetState() {
     }
 }
 
+//This function does a lot - starts by taking in an event and setting two variables, selected button and correct
+//also calls back functions setStatusClass and answerAlert
+//first checks if the length of the shuffled questions is greater than the lenfth of the current question index plus 1, in other words are we out of questions
+//then, if we are NOT out of questions, remove the hidden next button and carry on
+//else, if we are at the end of the questions, the total score is pulled in and there are thresholds that determine what final message and image is received
 function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
@@ -63,8 +88,7 @@ function selectAnswer(e) {
     answerAlert(e)
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
-        console.log(totalRunningScore)
-    } else if (totalRunningScore < 4) {
+    } else if (totalRunningScore < 3) {
         questionContainerElement.classList.add('hide')
         questionElement.classList.add('hide')
         answerButtonsElement.classList.add('hide')
@@ -72,7 +96,7 @@ function selectAnswer(e) {
         let image = document.getElementById("cat-image")
         image.src = "https://static.wixstatic.com/media/58d105_d0838e19f66d4c5d88517a3dc2066504~mv2.png/v1/fill/w_754,h_572,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/58d105_d0838e19f66d4c5d88517a3dc2066504~mv2.png"
         console.log(totalRunningScore)
-    } else if (totalRunningScore < 6) {
+    } else if (totalRunningScore < 4) {
         questionContainerElement.classList.add('hide')
         questionElement.classList.add('hide')
         answerButtonsElement.classList.add('hide')
@@ -80,7 +104,7 @@ function selectAnswer(e) {
         let image = document.getElementById("cat-image")
         image.src = "https://static.wixstatic.com/media/58d105_c869a439da5c4196be9082da0b23b526~mv2.png/v1/fill/w_696,h_768,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/58d105_c869a439da5c4196be9082da0b23b526~mv2.png"
         console.log(totalRunningScore)
-    } else if (totalRunningScore < 8) {
+    } else if (totalRunningScore < 6) {
         questionContainerElement.classList.add('hide')
         questionElement.classList.add('hide')
         answerButtonsElement.classList.add('hide')
@@ -88,7 +112,7 @@ function selectAnswer(e) {
         let image = document.getElementById("cat-image")
         image.src = "https://static.wixstatic.com/media/58d105_6b8553dd31454d169815baf422fdf005~mv2.png/v1/fill/w_728,h_768,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/58d105_6b8553dd31454d169815baf422fdf005~mv2.png"
         console.log(totalRunningScore)
-    } else if (totalRunningScore < 9) {
+    } else if (totalRunningScore < 8) {
         questionContainerElement.classList.add('hide')
         questionElement.classList.add('hide')
         answerButtonsElement.classList.add('hide')
@@ -107,6 +131,11 @@ function selectAnswer(e) {
     }
 }
 
+//this function is taking in an element and a correct status (true/false)
+//it calls the clear status function on the element
+//if the correct status is true, 'correct' is added back to the element as a class
+//else, "wrong" is added back to the element as a class
+//the answerAlert function is called taking the element as an argument
 function setStatusClass(element, correct) {
     clearStatusClass(element);
     if (correct) {
@@ -115,13 +144,15 @@ function setStatusClass(element, correct) {
         console.log(totalRunningScore)
     } else {
         element.classList.add('wrong')
-        console.log("check here on the else", totalRunningScore)
     }
     answerAlert(element)
 }
 
+//This function takes in an element
+//if the element classlist is wrong, it sets the answer button element class to btn wrong (with css styles set)
+//the h2 text is changed to red and the user gets a message saying it was wrong
+//else, the class is set to correct and the h2 text is set to green, and the text says they got it right
 function answerAlert(e) {
-    console.log(e.classList)
     if ( e.classList == 'wrong') {
         answerButtonsElement.classList = 'btn wrong'
         headerTwo.style = "color:red"
@@ -133,14 +164,19 @@ function answerAlert(e) {
     }
 }
 
+//This function resets some elements
+//first removes either 'correct' or 'wrong' from the element that was taken in as an argument
+//then the answer button element class is set back to the regular btn
+//h2 becomes purple again and the text is reset to nuetral
 function clearStatusClass(element) {
-    element.classList.remove('correct');
+    element.classList.remove('correct')
     element.classList.remove('wrong')
     answerButtonsElement.classList = 'btn'
     headerTwo.style = "color:purple"
     headerTwo.innerHTML = "Which Cat Are You?"
 }
 
+//This is an arracy of objects with nested arrays of objects that covers the 9 questions with two answers each
 const questions = [
     {
         question: 'What is the proper term for a group of kittens?',
@@ -207,6 +243,7 @@ const questions = [
     }
 ]
 
+//This is my fetch request that brings us a new kitty image every time a question is loaded
 function fetchCatImage() {
     let image = document.getElementById("cat-image")
     fetch('https://api.thecatapi.com/v1/images/search')
